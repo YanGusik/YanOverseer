812eda24538b5a8fd9dea3c88f0e1e9cb3650384
yan <minibaev2001@yandex.ru>
2020-07-23 18:51:51 +0400

    Add .gitattributes .gitignore

---
+++ YanOverseer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace YanOverseer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
